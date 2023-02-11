4d5e858e216a4be6844a2109beb921b48917d9ff
doprz <52579214+doprz@users.noreply.github.com>
2023-02-11 14:36:43 -0600

    Add BasicModal

--- content-scripts/contents/modal.tsx
+++ content-scripts/contents/modal.tsx
@@ -1,11 +1,18 @@
 import createCache from "@emotion/cache"
 import { CacheProvider } from "@emotion/react"
 import Box from "@mui/material/Box"
+import ScopedCssBaseline from '@mui/material/ScopedCssBaseline'
 import Typography from "@mui/material/Typography"
-import { createTheme } from "@mui/material/styles"
+import {
+    StyledEngineProvider,
+    ThemeProvider,
+    createTheme
+} from "@mui/material/styles"
 import type { PlasmoGetRootContainer } from "plasmo"
 import React from "react"
 import ReactDOM from "react-dom/client"
+
+import BasicModal from "../components/Modal"
 
 export const getRootContainer = () =>
     document.getElementById("utrp-content-root")
@@ -48,58 +55,62 @@ const theme = createTheme({
 
 ReactDOM.createRoot(shadowRootElement).render(
     <React.StrictMode>
+        <ScopedCssBaseline />
         <CacheProvider value={cache}>
-            <Box sx={{ width: "100%", maxWidth: 500 }}>
-                <Typography variant="h1" gutterBottom>
-                    h1. Heading
-                </Typography>
-                <Typography variant="h2" gutterBottom>
-                    h2. Heading
-                </Typography>
-                <Typography variant="h3" gutterBottom>
-                    h3. Heading
-                </Typography>
-                <Typography variant="h4" gutterBottom>
-                    h4. Heading
-                </Typography>
-                <Typography variant="h5" gutterBottom>
-                    h5. Heading
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                    h6. Heading
-                </Typography>
-                <Typography variant="subtitle1" gutterBottom>
-                    subtitle1. Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Quos blanditiis tenetur
-                </Typography>
-                <Typography variant="subtitle2" gutterBottom>
-                    subtitle2. Lorem ipsum dolor sit amet, consectetur
-                    adipisicing elit. Quos blanditiis tenetur
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    body1. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit. Quos blanditiis tenetur unde suscipit, quam beatae
-                    rerum inventore consectetur, neque doloribus, cupiditate
-                    numquam dignissimos laborum fugiat deleniti? Eum quasi
-                    quidem quibusdam.
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                    body2. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit. Quos blanditiis tenetur unde suscipit, quam beatae
-                    rerum inventore consectetur, neque doloribus, cupiditate
-                    numquam dignissimos laborum fugiat deleniti? Eum quasi
-                    quidem quibusdam.
-                </Typography>
-                <Typography variant="button" display="block" gutterBottom>
-                    button text
-                </Typography>
-                <Typography variant="caption" display="block" gutterBottom>
-                    caption text
-                </Typography>
-                <Typography variant="overline" display="block" gutterBottom>
-                    overline text
-                </Typography>
-            </Box>
+            <ThemeProvider theme={theme}>
+                <BasicModal />
+                <Box sx={{ width: "100%", maxWidth: 500 }}>
+                    <Typography variant="h1" gutterBottom>
+                        h1. Heading
+                    </Typography>
+                    <Typography variant="h2" gutterBottom>
+                        h2. Heading
+                    </Typography>
+                    <Typography variant="h3" gutterBottom>
+                        h3. Heading
+                    </Typography>
+                    <Typography variant="h4" gutterBottom>
+                        h4. Heading
+                    </Typography>
+                    <Typography variant="h5" gutterBottom>
+                        h5. Heading
+                    </Typography>
+                    <Typography variant="h6" gutterBottom>
+                        h6. Heading
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                        subtitle1. Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Quos blanditiis tenetur
+                    </Typography>
+                    <Typography variant="subtitle2" gutterBottom>
+                        subtitle2. Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Quos blanditiis tenetur
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                        body1. Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Quos blanditiis tenetur unde suscipit,
+                        quam beatae rerum inventore consectetur, neque
+                        doloribus, cupiditate numquam dignissimos laborum fugiat
+                        deleniti? Eum quasi quidem quibusdam.
+                    </Typography>
+                    <Typography variant="body2" gutterBottom>
+                        body2. Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Quos blanditiis tenetur unde suscipit,
+                        quam beatae rerum inventore consectetur, neque
+                        doloribus, cupiditate numquam dignissimos laborum fugiat
+                        deleniti? Eum quasi quidem quibusdam.
+                    </Typography>
+                    <Typography variant="button" display="block" gutterBottom>
+                        button text
+                    </Typography>
+                    <Typography variant="caption" display="block" gutterBottom>
+                        caption text
+                    </Typography>
+                    <Typography variant="overline" display="block" gutterBottom>
+                        overline text
+                    </Typography>
+                </Box>
+            </ThemeProvider>
         </CacheProvider>
     </React.StrictMode>
 )
